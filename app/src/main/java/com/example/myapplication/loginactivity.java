9ed8ced4f3ca93044461439b5911f9aeceb5d59a
protocorn <chordiasahil24@gmail.com>
2021-10-26 20:21:03 +0530

    login

--- app/src/main/java/com/example/myapplication/loginactivity.java
+++ app/src/main/java/com/example/myapplication/loginactivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class loginactivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class loginactivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginactivity);
+        TextView log_reg;
+        log_reg=findViewById(R.id.log_to_reg);
+
+        log_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(loginactivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
