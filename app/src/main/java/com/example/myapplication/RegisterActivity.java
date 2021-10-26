0fc1e97bde022d612716dee049bad21d15bdfab0
Asus <sampadabhadane@gmail.com>
2021-10-26 20:21:46 +0530

    reg to login

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-    }
+        TextView reg_to_login;
+        reg_to_login=findViewById(R.id.reg__to_login);
+        reg_to_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegisterActivity.this,loginactivity.class);
+                startActivity(intent);
+            }
+        });
+
+}
 }
