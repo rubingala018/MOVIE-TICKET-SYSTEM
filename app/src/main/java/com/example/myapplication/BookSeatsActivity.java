05043a9d2f174824639f820a811d3f55f1e411da
protocorn <chordiasahil24@gmail.com>
2021-12-02 17:37:07 +0530

    desc layout

--- app/src/main/java/com/example/myapplication/BookSeatsActivity.java
+++ app/src/main/java/com/example/myapplication/BookSeatsActivity.java
@@ -12,7 +12,8 @@ public class BookSeatsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_book_seats);
 
         getSupportActionBar().hide();
-          /*DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+        /*DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         Calendar c = Calendar.getInstance();
         c.add(Calendar.DATE, 1);
         Toast.makeText(MoviesDescActivity.this, dateFormat.format(c.getTime()), Toast.LENGTH_SHORT).show();*/
