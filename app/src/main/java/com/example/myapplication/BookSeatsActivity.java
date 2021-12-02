41e9fe860aeb732d158fcd437d429df5c5fda0c5
Asus <sampadabhadane@gmail.com>
2021-12-02 18:28:20 +0530

    book seats acc code

--- app/src/main/java/com/example/myapplication/BookSeatsActivity.java
+++ app/src/main/java/com/example/myapplication/BookSeatsActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DateFormat;
@@ -11,6 +14,10 @@ import java.util.Calendar;
 
 public class BookSeatsActivity extends AppCompatActivity {
     String title,photo_url,rating,lang,release_date,overview;
+    TextView date1, date2, date3, date4, date5, date6,date7;
+    TextView time1, time2, time3,time4,time5,time6,time7;
+    String theatre;
+    TextView movietitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +53,48 @@ public class BookSeatsActivity extends AppCompatActivity {
 
         Calendar c6 = Calendar.getInstance();
         c6.add(Calendar.DATE, 7);
+
+        date1=findViewById(R.id.d1);
+        date2=findViewById(R.id.d2);
+        date3=findViewById(R.id.d3);
+        date4=findViewById(R.id.d4);
+        date5=findViewById(R.id.d5);
+        date6=findViewById(R.id.d6);
+        date7=findViewById(R.id.d7);
+
+        time1=findViewById(R.id.t1);
+        time2=findViewById(R.id.t2);
+        time3=findViewById(R.id.t3);
+        time4=findViewById(R.id.t4);
+        time5=findViewById(R.id.t5);
+        time6=findViewById(R.id.t6);
+        time7=findViewById(R.id.t7);
+
+        movietitle.setText(title);
+        date1.setText(dateFormat.format(c.getTime()));
+        date2.setText(dateFormat.format(c1.getTime()));
+        date3.setText(dateFormat.format(c2.getTime()));
+        date4.setText(dateFormat.format(c3.getTime()));
+        date5.setText(dateFormat.format(c4.getTime()));
+        date6.setText(dateFormat.format(c5.getTime()));
+        date7.setText(dateFormat.format(c6.getTime()));
+
+        time1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(BookSeatsActivity.this,MainActivity.class);
+                intent.putExtra("Theatre","Theatre 1");
+                intent.putExtra("title", title);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
 
 
 
