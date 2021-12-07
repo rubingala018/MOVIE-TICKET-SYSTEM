b0dced50bfa2b01120a686aa84954e1aa3e2a606
Rubin Gala <rubingala018@gmail.com>
2021-12-07 17:02:39 +0530

    home page

--- app/src/main/java/com/example/myapplication/Fragments/ProfileFrag.java
+++ app/src/main/java/com/example/myapplication/Fragments/ProfileFrag.java
@@ -1,66 +1,81 @@
 package com.example.myapplication.Fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.databinding.ActivityPurchasehistoryBinding;
+import com.example.myapplication.databinding.FragmentBlankBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFrag#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ProfileFrag extends Fragment {
+    FirebaseDatabase database;
+    FirebaseAuth auth;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FragmentBlankBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+
+
 
     public ProfileFrag() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFrag.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFrag newInstance(String param1, String param2) {
-        ProfileFrag fragment = new ProfileFrag();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        database=FirebaseDatabase.getInstance();
+        auth=FirebaseAuth.getInstance();
+        binding=FragmentBlankBinding.inflate(inflater,container,false);
+        binding.logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+            }
+        });
+        database.getReference("User").child(auth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name=""+snapshot.child("username");
+                        String email=""+snapshot.child("email");
+                        binding.name.setText(name);
+                        binding.email1.setText(email);
+                        binding.username1.setText("hi"+name);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        binding.buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getContext(),Purc)
+            }
+        });
+
+        return binding.getRoot();
     }
 }
